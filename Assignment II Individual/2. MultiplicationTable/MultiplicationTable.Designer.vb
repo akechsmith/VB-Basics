d75f43e64e902decc57cdd3b3f7ba48434cca0d2
akech atem <97437834+akechsmith@users.noreply.github.com>
2024-04-13 12:31:56 +0300

    Add files via upload
    initial commit

---
+++ Assignment II Individual/2. MultiplicationTable/MultiplicationTable.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MultiplicationTable
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(186, 64)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(416, 297)
+        TextBox1.TabIndex = 0
+        ' 
+        ' MultiplicationTable
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Name = "MultiplicationTable"
+        Text = "Generate Multiplication Table"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Assignment II Individual/2. MultiplicationTable/MultiplicationTable.vb
@@ -0,0 +1,19 @@
+﻿Public Class MultiplicationTable
+    Private Sub MultiplicationTable_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GenerateMultiplicationTable()
+    End Sub
+    Private Sub GenerateMultiplicationTable()
+        Dim tableText As String = "Multiplication Table" & vbCrLf
+
+        For i As Integer = 1 To 7
+            For j As Integer = 1 To 7
+                tableText &= $"{i * j} "
+            Next
+            tableText &= vbCrLf
+        Next
+
+        TextBox1.Multiline = True ' Ensure TextBox is set to accept multiline text
+        TextBox1.Text = tableText
+    End Sub
+
+End Class
