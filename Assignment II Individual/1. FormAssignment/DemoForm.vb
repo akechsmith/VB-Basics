0dd06d891a363d5a055158c58b1f2e9758ebec0b
akech atem <97437834+akechsmith@users.noreply.github.com>
2024-04-13 12:26:37 +0300

    Add files via upload
    initial commit

---
+++ Assignment II Individual/1. FormAssignment/DemoForm.vb
@@ -0,0 +1,22 @@
+﻿Public Class DemoForm
+    Public Sub SetFormSize()
+        Dim screenWidth As Integer
+        Dim screenHeight As Integer
+
+        ' Get the screen dimensions
+        screenWidth = Screen.PrimaryScreen.Bounds.Width
+        screenHeight = Screen.PrimaryScreen.Bounds.Height
+
+        ' Set the form size
+        Me.Height = screenHeight \ 2 ' Set vertical size to half the screen height
+        Me.Width = screenWidth * 3 \ 4 ' Set horizontal size to three-quarters the screen width
+    End Sub
+    Private Sub DemoForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Call the SetFormSize subroutine to adjust the form size
+        SetFormSize()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
